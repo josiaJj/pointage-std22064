808a3fd61189ce4fde452de133c55565b525092e
josiaJj <hei.josia@gmail.com>
2024-06-19 15:13:39 +0300

    feat: add Category model

---
+++ src/main/java/school/hei/Category.java
@@ -0,0 +1,8 @@
+package school.hei;
+
+public class Category {
+    private String name;
+    private int normalWorkingHours;
+    private double weeklyNormalSalary;
+    private double compensationAmount;
+}
