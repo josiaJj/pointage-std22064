7e775f2e09bb468ef5c9cdc3f3449aed06777a8f
josiaJj <hei.josia@gmail.com>
2024-06-19 15:03:12 +0300

    feat: add Employee model

---
+++ src/main/java/school/hei/Employee.java
@@ -0,0 +1,13 @@
+package school.hei;
+
+import java.time.Instant;
+
+public class Employee {
+    private String name;
+    private int matriculeNumber;
+    private Instant birthDate;
+    private Instant contractStartDate;
+    private Instant contractEndDate;
+    private double salaryAmount;
+    private String category;
+}
